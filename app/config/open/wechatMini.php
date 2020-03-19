44e0e404be9365586f2b481e3ad10ec1820d5274
lusongsong2 <lusongsong@jd.com>
2020-03-19 18:38:09 +0800

    股票小程序配置

--- app/config/open/wechatMini.php
+++ app/config/open/wechatMini.php
@@ -1,7 +1,7 @@
 <?php
 //微信小程序配置
 return [
-    //路松松公众号
+    //1fox3小程序
     '1fox3' => [
         //mp.weixin.qq.com 登录账号
         'account' => 'php1fox3@gmail.com',
@@ -16,5 +16,19 @@ return [
         //appSecret
         'appSecret' => 'f8ab54b64c826480424d1b76dca73c44',
     ],
-	
+    //stock小程序
+    'stock' => [
+        //mp.weixin.qq.com 登录账号
+        'account' => 'zyjminiapp@126.com',
+        //登录密码
+        'password' => '',
+        //公众号名称
+        'openName' => 'stock cc',
+        //原始id
+        'originalId' => 'gh_92f9e40e895f',
+        //appId
+        'appId' => 'wx27ad45a61e944b65',
+        //appSecret
+        'appSecret' => '15120a6915577aa425bcdb272db54186',
+    ],
 ];
